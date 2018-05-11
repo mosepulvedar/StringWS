4f68aa40043068135d03b4955074df965dcd7fe5
1300931 <1300931>
2018-05-11 11:33:52 -0400

    primer commit

---
+++ src/java/ws/StringWS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author moises
+ */
+@WebService(serviceName = "StringWS")
+public class StringWS {
+
+    public String minusculas(String palabra) {
+        return palabra.toLowerCase();
+    }
+}
